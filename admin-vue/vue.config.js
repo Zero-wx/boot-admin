255c6533d5238fd80ee220a4d667879add8243d9
huangweixiang <huangwx313522@163.com>
2022-06-30 18:02:10 +0800

    vue相关配置

--- admin-vue/vue.config.js
+++ admin-vue/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        port: 3000,
+    },
+
+    // webpack 相关操作
+    // 1、将相关配置最终合并到Webpack
+    configureWebpack: config => {
+
+    },
+    // 2、对相关的webpack配置进行更加细粒度修改
+    chainWebpack(config) {
+
+    }
 })
+console.log(process.env.NODE_ENV)
