4bd31989688e8eae8ba43072be6bf45abe0c53e5
huangweixiang <huangwx313522@163.com>
2022-07-01 14:35:38 +0800

    初始化路由，登录，首页

--- admin-vue/src/router/index.js
+++ admin-vue/src/router/index.js
@@ -1,13 +1,28 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 
 const routes = [
-
+    {
+        path: '/',
+        redirect: '/dashboard',
+        children: [{
+            path: 'dashboard',
+            name: 'dashboard',
+            component: () => import('@/views/dashboard/index'),
+        }]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        meta: {title: 'login'},
+        component: () => import('@/views/login/index'),
+        hidden: true
+    },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
