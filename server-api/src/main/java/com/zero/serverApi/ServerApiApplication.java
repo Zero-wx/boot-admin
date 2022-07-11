b745f803509e0f5cd42386a30022b9dcc3245ecf
huangweixiang <huangwx313522@163.com>
2022-07-11 18:05:27 +0800

    修改

--- server-api/src/main/java/com/zero/serverApi/ServerApiApplication.java
+++ server-api/src/main/java/com/zero/serverApi/ServerApiApplication.java
@@ -1,13 +1,40 @@
 package com.zero.serverApi;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ServerApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServerApiApplication.class, args);
+
+    private static Logger logger = LoggerFactory.getLogger(ServerApiApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(ServerApiApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String active = env.getProperty("spring.profiles.active");
+        String port = env.getProperty("server.port");
+        port = port == null ? "8080" : port;
+        String path = env.getProperty("server.servlet.context-path");
+        path = path == null ? "" : path;
+        logger.info("\n----------------------------------------------------------\n\t" +
+                "web-flash is running! \n\t" +
+                "系统运行环境 : \t" + active + "\n\t" +
+                "本地访问地址 : \thttp://localhost:" + port + path + "/\n\t" +
+                "外部访问地址 : \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "在线文档地址 : \thttp://" + ip + ":" + port + path + "/swagger-ui/index.html\n" +
+                "----------------------------------------------------------");
+
     }
 
 }
--- server-api/src/test/java/com/zero/serverApi/ServerApiApplicationTests.java
+++ server-api/src/test/java/com/zero/serverApi/ServerApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.zero.serverApi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class ServerApiApplicationTests {
 
+    @Autowired(required = false)
+    private JdbcTemplate jdbcTemplate;
+
+
     @Test
     void contextLoads() {
+
+
+        String sql = "select * from t_user ";
+
+
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+
+
+        System.out.println(maps);
     }
 
 }
