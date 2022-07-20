96f6a1fd6b2f050b9fbe58d96740ebae043f71c2
huangweixiang <huangwx313522@163.com>
2022-07-20 18:01:11 +0800

    通知

--- admin-vue/src/views/login/index.vue
+++ admin-vue/src/views/login/index.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="login-container show">
         <router-link to="dashboard">/dashboard</router-link>
+
+        <div @click="clear">测试</div>
+
     </div>
 
 </template>
 
-<script>
+<script setup>
+import swal from "sweetalert";
+
+function clear() {
+    console.log("asd");
+    swal("", "Hello world!", "success", { buttons: false, timer: 2000 });
+}
+
 
 </script>
 
@@ -13,4 +23,5 @@
 .login-container {
     height: 100%;
 }
+
 </style>
