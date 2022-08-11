ae99c5f4978ec976e40e0fb9b31e342c220c3f1d
huangweixiang <huangwx313522@163.com>
2022-08-11 18:00:45 +0800

    请求拦截器

---
+++ server-api/src/main/java/com/zero/serverApi/interceptor/JwtInterceptor.java
@@ -0,0 +1,6 @@
+package com.zero.serverApi.interceptor;
+
+import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+
+public class JwtInterceptor extends BasicHttpAuthenticationFilter {
+}
